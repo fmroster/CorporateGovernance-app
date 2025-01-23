82e7a0575492e12b686d7ead77e4d7a6e76c6146
Frankie Mooketsi <84230693+fmroster@users.noreply.github.com>
2025-01-23 09:18:11 +0200

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import { routes } from 'routes'
+import { ThemeProvider } from './providers/theme-provider'
 
 export const App: React.FC = () => {
   return (
-    <RouterProvider router={createBrowserRouter(routes)} />
+    <ThemeProvider defaultTheme={'dark'}>
+      <RouterProvider router={createBrowserRouter(routes)} />
+    </ThemeProvider>
   )
 }
